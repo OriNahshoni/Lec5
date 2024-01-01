72a68d1088e695f8fc4602b4d23c2aa14f228214
OriNahshoni <139678044+OriNahshoni@users.noreply.github.com>
2024-01-01 15:36:03 +0200

    Initial commit

---
+++ src/database/users.ts
@@ -0,0 +1,84 @@
+export const users = [
+  {
+    name: {
+      first: "Bruce",
+      middle: "",
+      last: "Willis",
+    },
+    address: {
+      city: "Anytown",
+      state: "",
+      country: "Israel",
+      street: "123 Main St",
+      zip: "12345",
+      houseNumber: 20,
+    },
+    image: {
+      alt: "user-profile",
+      url: "https://picsum.photos/200/300",
+    },
+    phone: "050-8123091",
+    email: "Willis@batcave.com",
+    password: "$2b$12$4DvTND2MogN1unDiEjgxF.XbeUh8hevRzf.IojAHv4H6Gt6cMaXem",
+    isAdmin: false,
+    isBusiness: false,
+  },
+  {
+    name: {
+      first: "root",
+      middle: "",
+      last: "admin",
+    },
+    address: {
+      city: "Anytown",
+      state: "",
+      country: "Israel",
+      street: "123 Main St",
+      zip: "12345",
+      houseNumber: 20,
+    },
+    image: {
+      alt: "user-profile",
+      url: "https://picsum.photos/200/300",
+    },
+    phone: "050-8123091",
+    email: "admin@gmail.com",
+    password: "$2b$12$JLz5of1Ob2HMCBxb5BFONeqrgtumAqkeGuVe8A5aObj8ThbicO/Ne",
+    isAdmin: true,
+    isBusiness: true,
+  },
+  {
+    name: {
+      first: "Ori",
+      middle: "",
+      last: "Nahshoni",
+      _id: {
+        $oid: "658c33d06a38faa4996db0c4",
+      },
+    },
+    address: {
+      city: "Ein Habsor",
+      state: "",
+      country: "Israel",
+      street: "Shita",
+      zip: "12345",
+      houseNumber: 20,
+      _id: {
+        $oid: "658c33d06a38faa4996db0c5",
+      },
+    },
+    image: {
+      alt: "user-profile",
+      url: "https://picsum.photos/200/300",
+      _id: {
+        $oid: "658c33d06a38faa4996db0c6",
+      },
+    },
+    phone: "050-8123091",
+    email: "ori@gmail.com",
+    password: "$2b$12$aN3xmb4WCjTS9N3g2g/S9uommrQjneS5HD2ZlAbw2UHkT2mH.ei.m",
+    isAdmin: false,
+    isBusiness: true,
+    
+  }
+];
